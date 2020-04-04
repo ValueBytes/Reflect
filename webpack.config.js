74a245bd15c5e72284c23a0cc3aa92cd90bdd616
Hunter Keca <hunter@heysparkbox.com>
2020-04-04 14:15:35 -0500

    feat: Add simple react app
    - Add babel
    - Add html loader
    - Add default index.html template

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,23 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
