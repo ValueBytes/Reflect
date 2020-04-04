48bf945310538a351b38e3dd558e81b2de366169
Hunter Keca <hunter@heysparkbox.com>
2020-04-04 14:26:44 -0500

    feat (styles): add style configuration and base stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./styles/app.scss";
+
 const App = () => (
   <div>
     Blah
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,11 @@ module.exports = {
         use: {
           loader: 'html-loader'
         }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
