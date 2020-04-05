d0210c1657026b115f173abe33a59e37388b15e2
Hunter Keca <hunter@heysparkbox.com>
2020-04-05 14:29:55 -0500

    feat (navbar): connect nav to redux

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import Auth from './Auth.jsx';
+import { connect } from 'react-redux';
 
 class Navbar extends Component {
   render() {
+    const { user, signedIn } = this.props;
+    
     return (
       <div className="navbar">
         <h3 className="navbar__title">
@@ -10,16 +13,23 @@ class Navbar extends Component {
         </h3>
 
         <div className="navbar__profile">
-          <Auth />
+          <Auth show={signedIn} />
           <img
             className="navbar__profile-image" 
             src="https://via.placeholder.com/50"
           />
-          <p>Anonymous</p>
+          <p>{user.displayName}</p>
         </div>
       </div>
     );
   }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+    user: state.auth.user,
+    signedIn: state.auth.signedIn
+  };
+}
+
+export default connect(mapStateToProps)(Navbar);
