4c2ab5ee74a8cef174ce325c30a0ed8cdc5a96b4
Hunter Keca <hunter@heysparkbox.com>
2020-04-05 17:18:52 -0500

    feat (auth): Add github login

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import firebase from '../config/firebase';
 
 class Auth extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  async ghLogin() {
+    let provider = new firebase.auth.GithubAuthProvider();
+
+    try {
+      await firebase
+              .auth()
+              .signInWithPopup(provider);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   render() {
+    const { show } = this.props;
+
     return (
-      <div className="auth">
+      <div className={show ? "auth auth--hide" : "auth"}>
         <div className="auth__container">
           <h3 className="auth__title">Login</h3>
 
-          <button className="button button--primary">
+          <button className="button button--primary" onClick={this.ghLogin}>
             Log in with GitHub
           </button>
           <button className="button button--primary">
