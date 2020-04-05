8e51c0d776c444969f69256e62827b08ba1c984c
Hunter Keca <hunter@heysparkbox.com>
2020-04-05 17:19:17 -0500

    feat (auth): Add logout to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import Auth from './Auth.jsx';
 import { connect } from 'react-redux';
 
+import firebase from '../config/firebase';
+
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.signOut = this.signOut.bind(this);
+  }
+
+  async signOut() {
+    try {
+      await firebase.auth().signOut();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   render() {
     const { user, signedIn } = this.props;
     
@@ -18,7 +34,7 @@ class Navbar extends Component {
             className="navbar__profile-image" 
             src="https://via.placeholder.com/50"
           />
-          <p>{user.displayName}</p>
+          <p onClick={this.signOut}>{user.displayName}</p>
         </div>
       </div>
     );
