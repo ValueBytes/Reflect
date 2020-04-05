5b8e7c1cfc4e5e00038ee839c1b09f80d96bdc04
Hunter Keca <hunter@heysparkbox.com>
2020-04-05 17:19:40 -0500

    feat (actions): add setDefaultUser for setting a user when logged out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -5,6 +5,12 @@ const setUser = (user) => {
   store.dispatch({type: 'SIGNED_IN', payload: true});
 }
 
+const setDefaultUser = () => {
+  store.dispatch({type: 'SET_USER', payload: {displayName: 'Not signed in'}});
+  store.dispatch({type: 'SIGNED_IN', payload: false});
+}
+
 export {
-  setUser
+  setUser,
+  setDefaultUser
 }
