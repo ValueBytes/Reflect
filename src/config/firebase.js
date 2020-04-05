30c1b68372325a21fb378031adac0c8e3b0ee8f4
Hunter Keca <hunter@heysparkbox.com>
2020-04-04 22:52:57 -0500

    feat (firebase): Add firebase and firestore config

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbRIYd909JHYqIKCvpTSNdeC6ipiYhdyM",
+  authDomain: "reflect-a85a4.firebaseapp.com",
+  databaseURL: "https://reflect-a85a4.firebaseio.com",
+  projectId: "reflect-a85a4",
+  storageBucket: "reflect-a85a4.appspot.com",
+  messagingSenderId: "470361258575",
+  appId: "1:470361258575:web:4088744be82050c1e4083a"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.fireStore().settings({
+  timestampsInSnapshots: true
+});
+
+export default firebase;
