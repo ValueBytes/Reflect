43fe43e89319008efedc2270d285c05ffb39bce0
Hunter Keca <hunter@heysparkbox.com>
2020-04-05 14:27:25 -0500

    feat (store): Add auth default state, and action states

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,6 +1,27 @@
-const initState = {};
+const initState = {
+  user: {
+    displayName: 'Not signed in'
+  },
+  signedIn: false
+};
 
 const authReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'SET_USER': 
+      state = {
+        ...state,
+        user: action.payload
+      };
+      break;
+    
+    case 'SIGNED_IN':
+      state = {
+        ...state,
+        signedIn: action.payload
+      };
+      break;
+  }
+
   return state;
 };
 
