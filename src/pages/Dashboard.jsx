0fb5bd33d427546947715c278b1e91816a3a4689
Hunter Keca <hunter@heysparkbox.com>
2020-04-05 18:31:15 -0500

    feat: Add dashboard page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        blah
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
