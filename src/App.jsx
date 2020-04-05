fc63039315dd5f72f2f22037c4185f55f2a04085
Hunter Keca <hunter@heysparkbox.com>
2020-04-05 14:23:45 -0500

    fix (babel): add async runtime and corejs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+// https://stackoverflow.com/a/54490329
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import './styles/app.scss';
 import store from './store';
 import Navbar from './components/Navbar.jsx';
