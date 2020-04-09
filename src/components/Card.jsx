ce163772ad82debd2222e3d0d5c042de00edb703
Hunter Keca <hunter@heysparkbox.com>
2020-04-08 19:28:05 -0500

    feat: Update card to take in props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { board } = this.props;
+    const title = board.title;
+    const letter = board.title.substr(0, 1);
+
     return (
       <div className="card">
         <div className="card__icon">
-          W
+          {letter}
         </div>
 
-        <p className="card__title">waywo</p>
+        <p className="card__title">{title}</p>
       </div>
     );
   }
