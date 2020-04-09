bc03e4d333f03e628a0430755565282438ccb3a6
Hunter Keca <hunter@heysparkbox.com>
2020-04-08 19:27:42 -0500

    feat: connect dashboard page to state

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Card from '../components/Card.jsx';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { boards } = this.props;
+
     return (
       <div className="dashboard">
         <section className="dashboard__section">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <header>
+            <h2 className="dashboard__section-title">Public boards</h2>
+          </header>
+          <div className="dashboard__section-content">
+            {
+              boards.length > 0 &&
+              boards.map(b => (
+                <Card board={b} />
+              ))
+            }
+          </div>
         </section>
       </div>
     );
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    boards: state.dashboard.boards
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
