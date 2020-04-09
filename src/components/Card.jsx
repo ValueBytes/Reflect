06aca0a61520c7a48f921e1fd3ee5e0c6f68e288
Hunter Keca <hunter@heysparkbox.com>
2020-04-08 19:00:32 -0500

    feat: Add board card

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card__icon">
+          W
+        </div>
+
+        <p className="card__title">waywo</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
