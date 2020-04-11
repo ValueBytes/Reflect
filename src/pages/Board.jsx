215c40e715d1c42c1cf8f97cd9539e93130e3a48
Hunter Keca <hunter@heysparkbox.com>
2020-04-11 00:09:04 -0500

    refactor: Update board to use board component

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import Board from '../components/Board/index.jsx';
+import firebase from '../config/firebase';
 
-class Board extends Component {
+const db = firebase.firestore();
+
+class BoardDetails extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      ref: null,
+      board: null
+    };
+  }
+
+  componentDidMount() {
+    const board = this.props.match.params.id;
+
+    let ref = db.collection('boards')
+      .doc(board)
+      .onSnapshot(doc => {
+        this.setState({
+          board: doc.data()
+        });
+      });
+    
+    this.setState({
+      ref
+    });
+  }
+
+  componentWillUnmount() {
+    const { ref } = this.state;
+
+    // Disable realtime when destroying component
+    ref();
   }
 
   render() {
+    const { board } = this.state;
+
     return (
-      <div>
-        {this.props.columns[0].name}
-      </div>
+      <Board board={board} />
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    columns: state.board.columns
-  };
-}
-
-export default connect(mapStateToProps)(Board);
+export default BoardDetails;
