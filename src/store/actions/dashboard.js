60d95014ddcf6c820e88d9a6b65451240b8c1083
Hunter Keca <hunter@heysparkbox.com>
2020-04-10 21:11:52 -0500

    fix: Check for user id before trying to call api

--- src/store/actions/dashboard.js
+++ src/store/actions/dashboard.js
@@ -3,11 +3,20 @@ import firebase from '../../config/firebase';
 const db = firebase.firestore();
 
 export function getBoards() {
-  return async (dispatch) => {
-    let request = await db.collection('boards').get();
+  return async (dispatch, getState) => {
+    const userID = getState().auth.user.uid;
+
+    if (!userID)
+      dispatch({type: 'SET_ERROR', payload: 'no user.'});
+      
+    let boardsRef = await db.collection('boards');
+    let userBoards = await boardsRef
+      .where('uid', '==', userID)
+      .get();
+
     let boards = [];
 
-    request.forEach((doc) => {
+    userBoards.forEach((doc) => {
         boards.push({
           id: doc.id,
           ...doc.data()
