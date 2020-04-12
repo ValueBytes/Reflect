7fdd688222806e747292816439ff2ce44dc00876
Hunter Keca <hunter@heysparkbox.com>
2020-04-11 19:37:05 -0500

    refactor: remove redux connection

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,49 +1,27 @@
 import React, { Component } from 'react';
 import Board from '../components/Board/index.jsx';
-import firebase from '../config/firebase';
-
-const db = firebase.firestore();
+import { connect } from 'react-redux';
 
 class BoardDetails extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      ref: null,
-      board: null
-    };
-  }
-
-  componentDidMount() {
-    const board = this.props.match.params.id;
-
-    let ref = db.collection('boards')
-      .doc(board)
-      .onSnapshot(doc => {
-        this.setState({
-          board: doc.data()
-        });
-      });
-    
-    this.setState({
-      ref
-    });
-  }
-
-  componentWillUnmount() {
-    const { ref } = this.state;
-
-    // Disable realtime when destroying component
-    ref();
   }
 
   render() {
-    const { board } = this.state;
+    const { columns } = this.props;
 
     return (
-      <Board board={board} />
+      <Board 
+        columns={columns}
+      />
     );
   }
 }
 
-export default BoardDetails;
+const mapStateToProps = (state) => {
+  return {
+    columns: state.board.columns,
+  };
+}
+
+export default connect(mapStateToProps)(BoardDetails);
