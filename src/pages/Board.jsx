2d442ccfc85fd3c559c0e22765bdb028e6cdc52c
Hunter Keca <hunter@heysparkbox.com>
2020-04-13 22:36:19 -0500

    feat: connect board to redux

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import Board from '../components/Board/index.jsx';
 import { connect } from 'react-redux';
+import { 
+  setBoardListener,
+  removeBoardListener,
+  setItemListener,
+  removeItemListener
+} from '../store/actions/board';
 
 class BoardDetails extends Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    const { match, setListener, setItemListener } = this.props;
+    
+    setListener(match.params.id);
+    setItemListener(match.params.id);
+  }
+
+  componentWillUnmount() {
+    const { removeListener, removeItemListener } = this.props;
+    removeListener();
+    removeItemListener();
   }
 
   render() {
@@ -24,4 +43,21 @@ const mapStateToProps = (state) => {
   };
 }
 
-export default connect(mapStateToProps)(BoardDetails);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setListener: (id) => {
+      dispatch(setBoardListener(id));
+    },
+    removeListener: () => {
+      dispatch(removeBoardListener());
+    },
+    setItemListener: (id) => {
+      dispatch(setItemListener(id));
+    },
+    removeItemListener: () => {
+      dispatch(removeItemListener());
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoardDetails);
