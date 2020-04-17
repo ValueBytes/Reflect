76a28b95949fc2bd1bf3192e8603bbec1c2a8a13
Hunter Keca <hunter@heysparkbox.com>
2020-04-16 20:02:32 -0500

    feat: Add editable component

---
+++ src/components/Editable.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+class Editable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: props.defaultText,
+      editing: false
+    };
+
+    this.getPlainView = this.getPlainView.bind(this);
+    this.getEditView = this.getEditView.bind(this);
+    this.toggleEdit = this.toggleEdit.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  toggleEdit() {
+    const { editing } = this.state;
+
+    this.setState({
+      editing: !editing
+    });
+  }
+
+  getPlainView() {
+    const { text } = this.state;
+
+    return <p onDoubleClick={this.toggleEdit}>{text}</p>;
+  }
+
+  submit(e) {
+    if (e.target.value === "") {
+      this.toggleEdit();
+      this.setState({
+        text: this.props.defaultText
+      });
+      
+      alert('Board item must not be empty');
+      return;
+    }
+    
+    this.props.update(e.target.value);
+    this.toggleEdit();
+  }
+
+  getEditView() {
+    const { text } = this.state;
+
+    return (
+      <textarea
+        autoFocus
+        onBlur={this.submit}
+        value={text}
+        onChange={(e) => this.setState({text: e.target.value})}
+      >
+      </textarea>
+    );
+  }
+
+  render() {
+    const { editing } = this.state;
+
+    return editing 
+      ? this.getEditView() 
+      : this.getPlainView();
+  }
+}
+
+export default Editable;
